e823ec75e7f4e0f55ad292b6a94920df22d3d799
nuryudha <nur.yudha16@gmail.com>
2022-02-27 14:32:10 +0700

    Menyelesaikan Praktikum section 5

---
+++ 5_Time-Complexity-dan-Space-Complexity/Praktikum/Bilanganpangkat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bilanganpangkat {
+    public static void main(String[] args) {
+        int a,b,hasil;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Bilangan:");
+        a=input.nextInt();
+
+        System.out.print("Pangkat:");
+        b=input.nextInt();
+
+        hasil=(int) Math.pow(a, b);
+
+        System.out.println("Hasil :"+hasil);
+    }
+}
