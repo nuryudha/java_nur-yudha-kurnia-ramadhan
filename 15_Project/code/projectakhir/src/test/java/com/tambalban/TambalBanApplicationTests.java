57844289e09a81b3934c628ec05586182ece41e6
nuryudha <nur.yudha16@gmail.com>
2022-05-11 15:05:33 +0700

    membuat springboot

---
+++ 15_Project/code/projectakhir/src/test/java/com/tambalban/TambalBanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tambalban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TambalBanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
